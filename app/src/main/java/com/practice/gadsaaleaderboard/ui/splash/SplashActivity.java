68cb03ab38ec97eebe85c3df103d67f3fc102e9f
ShukranIsaac <imwakabira@ymail.com>
2020-09-09 23:13:29 +0200

    App Components: SplashActivity => delay by 2sec, b4 starting MainActivity.class

--- app/src/main/java/com/practice/gadsaaleaderboard/ui/splash/SplashActivity.java
+++ app/src/main/java/com/practice/gadsaaleaderboard/ui/splash/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.practice.gadsaaleaderboard.ui.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.practice.gadsaaleaderboard.MainActivity;
 import com.practice.gadsaaleaderboard.R;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +16,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DataBindingUtil.setContentView(this, R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, 2000);
     }
+
 }
